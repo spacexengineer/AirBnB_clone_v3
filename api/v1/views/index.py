b1f8fa0cd88361aa14930465d66d734c7ab8a346
spacexengineer <hunter.casbeer@gmail.com>
2017-09-08 09:08:47 -0700

    init feature to retrieve the number of object by text json format

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,30 @@
 """routes status of app"""
 from api.v1.views import app_views
 from flask import Flask, render_template, jsonify
+import models
+from models import storage
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app_views.route('/status')
 def status():
     """returns status in json format"""
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', methods=['GET'])
+def count_objs():
+    """retrieves number of objects by type"""
+    json_cls = ['amenities', 'cities', 'places', 'reviews', 'states', 'users']
+
+    # manually find the classes w/ this list, however, method below is better
+    # cls = ['Amentity', 'City', 'Place', 'Review', 'State', 'User']
+
+    class_models = sorted(models.CNC)
+    class_models.remove('BaseModel')
+
+    cls_count = [storage.count(i) for i in class_models]
+    obj_dict = {k: v for k, v in zip(json_cls, cls_count)}
+    return jsonify(obj_dict)

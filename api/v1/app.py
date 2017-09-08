159b525c218e7f8449dbb4a6c596e73654250436
spacexengineer <hunter.casbeer@gmail.com>
2017-09-08 11:01:55 -0700

    add env variables for host and port

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,4 +21,6 @@ def page_not_found(e):
 
 if __name__ == '__main__':
     """Where the Flask runs"""
-    app.run(host='0.0.0.0', port=5000)
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = os.getenv("HBNB_API_PORT", "5000")
+    app.run(host, port)
